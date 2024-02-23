73a34b0dbaa7f035ebe77d2086da54aeaf947c59
balmore <balmoreavalos2@gmail.com>
2024-02-23 14:06:47 +0100

    fin

--- src/main/resources/static/scripts/inicio.js
+++ src/main/resources/static/scripts/inicio.js
@@ -30,18 +30,24 @@ function getCiudades() {
 	});
 }
 function asignarCiudades(ciudades) {
+	$("#ciudadOrigen").append("<option>" + "ciudad de origen..." + "</option>");
+	$("#ciudadDestino").append("<option>" + "ciudad de destino..." + "</option>");
 	ciudades.forEach(ciudad => {
 		$("#ciudadOrigen").append("<option>" + ciudad + "</option>");
 		$("#ciudadDestino").append("<option>" + ciudad + "</option>");
 	});
 }
 function mostrarRuta(response) {
-	$("#ciudadOrigen").val("");
-	$("#ciudadDestino").val("");
+	$("#ciudadOrigen").val("ciudad de origen...");
+	$("#ciudadDestino").val("ciudad de destino...");
 	let rutaOptimaHTML = "";
-
+	let cont = 0;
 	response.forEach(ciudad => {
-		rutaOptimaHTML += ciudad + " ";
+		if (cont < response.length-1) {
+			rutaOptimaHTML += ciudad + " -> " + response[cont + 1]+" ";
+			cont++;
+		}
+
 	});
 
 	$("#rutaOptima").html(rutaOptimaHTML);
